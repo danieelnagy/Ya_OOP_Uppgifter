b1f9fff27df21c0baf31cc3fa1480b6e773a7e0e
danieelnagy <daniel.nagyblabla@icloud.com>
2020-11-21 04:44:31 +0100

    Format

--- uppgift2_Daniel_Nagy/Husse.java
+++ uppgift2_Daniel_Nagy/Husse.java
@@ -3,27 +3,23 @@ package uppgift2_Daniel_Nagy;
 import java.util.ArrayList;
 
 public class Husse {
-	
+
 	String husseName;
 	static ArrayList<Dog> dog = new ArrayList<Dog>();
-	
-	public Husse(String _husseName)
-	{
+
+	public Husse(String _husseName) {
 		husseName = _husseName;
 	}
-	
-	void SetAttributes()
-	{
-		for(int i = 0;i < 5;i++)
-		{
+
+	void SetAttributes() {
+		for (int i = 0; i < 5; i++) {
 			Dog doggy = new Dog("Goodboi", "Doggos");
 			dog.add(doggy);
 		}
 	}
-	void PrintAnimal()
-	{
-		for(var i : dog)
-		{
+
+	void PrintAnimal() {
+		for (var i : dog) {
 			System.out.println(husseName + " has some " + i.GetName() + " " + i.GetBreed());
 		}
 	}
--- uppgift2_Daniel_Nagy/Matte.java
+++ uppgift2_Daniel_Nagy/Matte.java
@@ -1,20 +1,20 @@
 package uppgift2_Daniel_Nagy;
 
 public class Matte {
-	
+
 	String name;
 	String catName = "HisCat";
 	Cat mattesCat = new Cat(catName, "Perser");
-	public Matte(String name)
-	{
+
+	public Matte(String name) {
 		this.name = name;
 	}
-	String GetCatName()
-	{
+
+	String GetCatName() {
 		return catName;
 	}
-	String GetInfo()
-	{
+
+	String GetInfo() {
 		return name + " " + mattesCat.GetName() + " " + mattesCat.GetBreed();
 	}
 }
