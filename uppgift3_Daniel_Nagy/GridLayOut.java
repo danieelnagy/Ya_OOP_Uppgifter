d6708409c2f36e4130f918ec00ec9c232a5ec63f
danieelnagy <daniel.nagyblabla@icloud.com>
2020-12-06 12:46:33 +0100

    rensar i koden

--- uppgift3_Daniel_Nagy/GridLayOut.java
+++ uppgift3_Daniel_Nagy/GridLayOut.java
@@ -1,10 +1,5 @@
 package uppgift3_Daniel_Nagy;
 
-import java.awt.Button;
-import java.time.Duration;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import javafx.animation.AnimationTimer;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -13,46 +8,45 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 
 class GridLayOut {
-	
+
 	private Elements elements = new Elements();
 	private GridPane grid;
 	private int numCols;
 	private int numRows;
-	
-	public GridLayOut()
-	{
-		Grid();
+
+	public GridLayOut() {
+		GridStyles();
 	}
-	private void Grid()
-	{
+
+	public GridPane getGrid() {
+		return grid;
+	}
+
+	private void GridStyles() {
 		grid = new GridPane();
 		grid.add(elements.getTimeLabel(), 0, 0);
 		grid.add(elements.getStartButton(), 0, 0);
 		grid.add(elements.getStopButton(), 0, 1);
-		grid.add(elements.getDateLabel(), 0, 2);		
+		grid.add(elements.getDateLabel(), 0, 2);
 		GridPane.setHalignment(elements.getStartButton(), HPos.RIGHT);
 		GridPane.setHalignment(elements.getStopButton(), HPos.CENTER);
 		GridPane.setHalignment(elements.getDateLabel(), HPos.CENTER);
 		grid.setMinSize(400, 200);
 		grid.setGridLinesVisible(true);
 		grid.setPadding(new Insets(15, 15, 15, 15));
-		grid.setVgap(5); 
-	    grid.setHgap(5); 
+		grid.setHgap(5);
 		grid.setAlignment(Pos.CENTER);
-        numCols = 1;
-        numRows = 3;
-        for (int i = 0; i < numCols; i++) {
-            ColumnConstraints colConst = new ColumnConstraints();
-            colConst.setPercentWidth(100 / numCols);
-            grid.getColumnConstraints().add(colConst);
-        }
-        for (int i = 0; i < numRows; i++) {
-            RowConstraints rowConst = new RowConstraints();
-            rowConst.setPercentHeight(100 / numRows);
-            grid.getRowConstraints().add(rowConst);         
-        }
-	}
-	public GridPane getGrid() {
-	    return grid;
+		numCols = 1;
+		numRows = 3;
+		for (int i = 0; i < numCols; i++) {
+			ColumnConstraints colConst = new ColumnConstraints();
+			colConst.setPercentWidth(100 / numCols);
+			grid.getColumnConstraints().add(colConst);
+		}
+		for (int i = 0; i < numRows; i++) {
+			RowConstraints rowConst = new RowConstraints();
+			rowConst.setPercentHeight(100 / numRows);
+			grid.getRowConstraints().add(rowConst);
+		}
 	}
 }
